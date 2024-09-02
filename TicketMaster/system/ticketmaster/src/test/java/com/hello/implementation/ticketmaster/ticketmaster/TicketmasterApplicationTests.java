95a380302a23af807e804803be14bb7956f963dc
Nico <negretenico@gmail.com>
2024-09-02 09:04:54 -0400

    Adding the starting points for each svc

---
+++ TicketMaster/system/ticketmaster/src/test/java/com/hello/implementation/ticketmaster/ticketmaster/TicketmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.implementation.ticketmaster.ticketmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
