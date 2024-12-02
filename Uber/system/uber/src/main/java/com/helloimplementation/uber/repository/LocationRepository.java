c0cc32ca0b743c3d5e0508a60cf23c459189a0df
Nico <negretenico@gmail.com>
2024-12-02 16:59:19 -0500

    Adding the driver endpooint

--- Uber/system/uber/src/main/java/com/helloimplementation/uber/repository/LocationRepository.java
+++ Uber/system/uber/src/main/java/com/helloimplementation/uber/repository/LocationRepository.java
@@ -2,6 +2,12 @@ package com.helloimplementation.uber.repository;
 
 import com.helloimplementation.uber.model.database.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location,Integer> {
+    @Query("SELECT l FROM Location l WHERE ABS(l.latitude - :latitude) < 0.0001 AND ABS(l.longitude - :longitude) < 0.0001")
+    Optional<Location> findLocationByLatAndLong(@Param("latitude") float latitude, @Param("longitude") float longitude);
 }
--- Uber/system/uber/src/main/java/com/helloimplementation/uber/service/location/LocationService.java
+++ Uber/system/uber/src/main/java/com/helloimplementation/uber/service/location/LocationService.java
@@ -6,6 +6,7 @@ import com.helloimplementation.uber.repository.LocationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @Service
 public class LocationService {
@@ -14,10 +15,17 @@ public class LocationService {
         this.locationRepository=locationRepository;
     }
     public Result<Location> getById(int id){
-        Optional<Location> possibleLocation = locationRepository.findById(id);
-        if(possibleLocation.isEmpty()){
-            return Result.failure(String.format("We could not find the location with id=%s",id));
-        }
-        return Result.success(possibleLocation.get());
+        return createResult(()->locationRepository.findById(id),String.format("We could not find the location with id=%s",id));
     }
+    public Result<Location> findByLatAndLong(float lat, float longitude){
+        return createResult(()->locationRepository.findLocationByLatAndLong(lat,longitude),String.format("We could not find the location with lat=%s and long=%s",lat,longitude));
+    }
+    private Result<Location> createResult(Supplier<Optional<Location>> supplier, String errorMsg) {
+        Optional<Location> result = supplier.get();
+        if (result.isEmpty()) {
+            return Result.failure(errorMsg );
+        }
+        return Result.success(result.get());
+    }
+
 }
